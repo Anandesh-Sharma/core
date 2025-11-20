d851649b03390b1a5d664e60c79b4f5edac886fd
Erik <erik@montnemery.com>
2025-11-20 11:55:50 +0100

    Add additional media_player triggers

--- tests/components/media_player/test_trigger.py
+++ tests/components/media_player/test_trigger.py
@@ -36,6 +36,27 @@ async def target_media_players(hass: HomeAssistant) -> None:
     ("trigger", "states"),
     [
         *parametrize_trigger_states(
+            trigger="media_player.paused_playing",
+            target_states=[
+                MediaPlayerState.PAUSED,
+            ],
+            other_states=[
+                MediaPlayerState.PLAYING,
+            ],
+        ),
+        *parametrize_trigger_states(
+            trigger="media_player.started_playing",
+            target_states=[
+                MediaPlayerState.PLAYING,
+            ],
+            other_states=[
+                MediaPlayerState.IDLE,
+                MediaPlayerState.OFF,
+                MediaPlayerState.ON,
+                MediaPlayerState.PAUSED,
+            ],
+        ),
+        *parametrize_trigger_states(
             trigger="media_player.stopped_playing",
             target_states=[
                 MediaPlayerState.IDLE,
@@ -46,6 +67,32 @@ async def target_media_players(hass: HomeAssistant) -> None:
                 MediaPlayerState.BUFFERING,
                 MediaPlayerState.PAUSED,
                 MediaPlayerState.PLAYING,
+            ],
+        ),
+        *parametrize_trigger_states(
+            trigger="media_player.turned_off",
+            target_states=[
+                MediaPlayerState.OFF,
+            ],
+            other_states=[
+                MediaPlayerState.BUFFERING,
+                MediaPlayerState.IDLE,
+                MediaPlayerState.ON,
+                MediaPlayerState.PAUSED,
+                MediaPlayerState.PLAYING,
+            ],
+        ),
+        *parametrize_trigger_states(
+            trigger="media_player.turned_on",
+            target_states=[
+                MediaPlayerState.BUFFERING,
+                MediaPlayerState.IDLE,
+                MediaPlayerState.ON,
+                MediaPlayerState.PAUSED,
+                MediaPlayerState.PLAYING,
+            ],
+            other_states=[
+                MediaPlayerState.OFF,
             ],
         ),
     ],
